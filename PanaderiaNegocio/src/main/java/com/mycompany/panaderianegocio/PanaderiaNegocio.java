8f61e11af3cbd5da1b96ec449ad644564c1bd75e
RubenRamR <160176918+RubenRamR@users.noreply.github.com>
2024-10-20 16:24:18 -0700

    Subsistema, Negocio

---
+++ PanaderiaNegocio/src/main/java/com/mycompany/panaderianegocio/PanaderiaNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.panaderianegocio;
+
+/**
+ *
+ * @author rramirez
+ */
+public class PanaderiaNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
