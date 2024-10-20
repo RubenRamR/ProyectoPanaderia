1c9d613fd551b30e8a3432405074457c598800c1
RubenRamR <160176918+RubenRamR@users.noreply.github.com>
2024-10-20 15:56:40 -0700

    Estructura

--- PanaderiaDTOs/src/main/java/Pruebas/PanaderiaNegocio.java
+++ PanaderiaDTOs/src/main/java/Pruebas/PanaderiaNegocio.java
@@ -2,15 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.panaderiapersistencia;
+package Pruebas;
 
 /**
  *
  * @author rramirez
  */
-public class PanaderiaPersistencia {
+public class PanaderiaNegocio {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
+    
--- PanaderiaPersistencia/src/main/java/Pruebas/PanaderiaPersistencia.java
+++ PanaderiaPersistencia/src/main/java/Pruebas/PanaderiaPersistencia.java
@@ -2,16 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.panaderianegocio;
+package Pruebas;
 
 /**
  *
  * @author rramirez
  */
-public class PanaderiaNegocio {
+public class PanaderiaPersistencia {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
-    
